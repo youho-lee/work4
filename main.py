df94948b18206640d3603c12695455a175f5948a
jun2151 <jun2151@gmail.com>
2023-11-01 14:44:50 +0900

    hotfix on main.py

--- main.py
+++ main.py
@@ -29,8 +29,9 @@ def main():
     # Example list
     int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    # Wrongly written code
-    output = sum_of_squares_of_even(int_list)
+    # Fixed Wrongly written code
+    output = even_list(int_list)
+    output = sum_of_squares_of_even(output)
     print(output)
 
 # Boilerplate code
